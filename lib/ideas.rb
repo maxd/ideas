c1dbeceb74020ac89e0ce77d86e6d49ff8d53ddb
Maxim Dobryakov <closer.main@gmail.com>
2012-09-15 16:38:51 +0400

    Simplify requiring of all files

--- lib/ideas.rb
+++ lib/ideas.rb
@@ -1,13 +1,1 @@
-require "ideas/version"
-
-require "ideas/models/feature"
-require "ideas/models/feature_set"
-require "ideas/models/idea"
-
-require "ideas/scopes/root_scope"
-require "ideas/scopes/idea_scope"
-require "ideas/scopes/feature_scope"
-
-require "ideas/helpers"
-require "ideas/loader"
-
+Dir[File.expand_path('../**/*.rb', __FILE__)].each{ |f| require f }
