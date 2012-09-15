12b39f2e2d85f57edde52bb4df8d76d13a5f8615
Maxim Dobryakov <closer.main@gmail.com>
2012-09-16 00:47:20 +0400

    Fix problem with initializing modules during loading

--- lib/ideas/cli/application.rb
+++ lib/ideas/cli/application.rb
@@ -1,38 +1,40 @@
 require 'cri'
 
-module Ideas::CLI
-  class Application
+module Ideas
+  module CLI
+    class Application
 
-    def initialize
-      filename = File.expand_path('../commands/ideas.rb', __FILE__)
-      @root_command = load_command_at(filename)
-    end
-
-    def run(args)
-      load_commands
-      @root_command.run(args)
-    end
-
-  private
-
-    def load_command_at(filename)
-      code = File.read(filename)
-      command = Cri::Command.define(code, filename)
-
-      command_name = File.basename(filename, '.rb')
-      command.modify { name command_name }
-
-      command
-    end
-
-    def load_commands
-      command_filenames = Dir[File.expand_path('../commands/*.rb', __FILE__)]
-      command_filenames.each do |filename|
-        next if File.basename(filename, '.rb') == 'ideas'
-        cmd = load_command_at(filename)
-        @root_command.add_command(cmd)
+      def initialize
+        filename = File.expand_path('../commands/ideas.rb', __FILE__)
+        @root_command = load_command_at(filename)
       end
-    end
 
+      def run(args)
+        load_commands
+        @root_command.run(args)
+      end
+
+    private
+
+      def load_command_at(filename)
+        code = File.read(filename)
+        command = Cri::Command.define(code, filename)
+
+        command_name = File.basename(filename, '.rb')
+        command.modify { name command_name }
+
+        command
+      end
+
+      def load_commands
+        command_filenames = Dir[File.expand_path('../commands/*.rb', __FILE__)]
+        command_filenames.each do |filename|
+          next if File.basename(filename, '.rb') == 'ideas'
+          cmd = load_command_at(filename)
+          @root_command.add_command(cmd)
+        end
+      end
+
+    end
   end
 end
--- lib/ideas/helpers.rb
+++ lib/ideas/helpers.rb
@@ -1,15 +1,17 @@
-module Ideas::Helpers
+module Ideas
+  module Helpers
 
-  def self.context_eval(context, *args)
-    options = args.last.is_a?(Hash) ? args.pop : {}
+    def self.context_eval(context, *args)
+      options = args.last.is_a?(Hash) ? args.pop : {}
 
-    if options[:string]
-      context.instance_eval(options[:string], options[:name] || '(eval)')
+      if options[:string]
+        context.instance_eval(options[:string], options[:name] || '(eval)')
 
-    elsif options[:proc]
-      context.instance_eval(&options[:proc])
+      elsif options[:proc]
+        context.instance_eval(&options[:proc])
 
+      end
     end
-  end
 
+  end
 end
